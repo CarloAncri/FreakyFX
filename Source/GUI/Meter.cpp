23ad35cdba3725bc3b2d075fe54e1590d609651d
CarloAncri <carlo.ancri@gmail.com>
2026-09-21 22:51:51 +0200

    added peak indicator

--- Source/GUI/Meter.cpp
+++ Source/GUI/Meter.cpp
@@ -1,12 +1,17 @@
 #include "Meter.h"
 
-
 void Meter::updateLevel(float newRawLevel)
 {
   if (newRawLevel > sigLvl)
     sigLvl = newRawLevel;
   else
     sigLvl *= alphaDecay;
+
+  if (newRawLevel >= clipThreshold)
+  {
+    isClipping = true;
+    startTimer(2000);
+  }
 
   repaint();
 }
@@ -16,15 +21,27 @@ void Meter::paint(juce::Graphics &g)
 {
   auto bounds = getLocalBounds().toFloat();
 
+  auto clipArea = bounds.removeFromTop(6.0f);
+  bounds.removeFromTop(3.0f);
+
+  auto metArea = bounds;
+
   g.setColour(juce::Colours::darkgrey);
-  g.fillRoundedRectangle(bounds, 3.0f);
+  g.fillRoundedRectangle(metArea, 3.0f);
+
+  if (isClipping)
+    g.setColour(juce::Colours::red);
+  else
+    g.setColour(juce::Colours::darkgrey.darker(0.8f));
+
+  g.fillRoundedRectangle(clipArea, 2.0f);
 
   float lvlDb = juce::Decibels::gainToDecibels(sigLvl, -60.0f);
   float mappedLvl = juce::jmap(lvlDb, -60.0f, 6.0f, 0.0f, 1.0f);
   mappedLvl = juce::jlimit(0.0f, 1.0f, mappedLvl);
 
-  float meterH = bounds.getHeight() * mappedLvl;
-  auto meterFill = bounds.removeFromBottom(meterH);
+  float meterH = metArea.getHeight() * mappedLvl;
+  auto meterFill = metArea.removeFromBottom(meterH);
 
   juce::ColourGradient gradient(
     juce::Colours::green, bounds.getBottomLeft(),
@@ -33,4 +50,12 @@ void Meter::paint(juce::Graphics &g)
 
   g.setGradientFill(gradient);
   g.fillRoundedRectangle(meterFill, 3.0f);
+}
+
+
+void Meter::timerCallback()
+{
+  isClipping = false;
+  stopTimer();
+  repaint();
 }
--- Source/GUI/Meter.h
+++ Source/GUI/Meter.h
@@ -7,18 +7,22 @@
  *
  * The decay factor is costant and not adjustable.
  */
-class Meter : public juce::Component
+class Meter : public juce::Component, public juce::Timer
 {
 public:
   Meter() = default;
   ~Meter() override = default;
 
   void updateLevel(float newRawLevel);
+  void timerCallback() override;
 
   void paint(juce::Graphics &g) override;
 private:
   float sigLvl = 0.0f;
   const float alphaDecay = 0.85f;
+
+  bool isClipping = false;
+  const float clipThreshold = 1.0f;
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(Meter)
 };
